8da65a7dde0887f3e28aa0f4c714de71a63e7964
Zd3n1 <109799398+Zd3n1@users.noreply.github.com>
2025-11-21 08:39:39 +0100

    OpenCV works - penguin 🐧

--- README.md
+++ README.md
@@ -2,5 +2,10 @@ Requiremnts for OpenCV
 - OpenCV libraries
 - data structures
 
+2 versions OpenCV libraries
+- C++
+- Python
 
+
+General idea is that you use functions writen in C++ in otherwise Python programs
 
---
+++ main.py
@@ -0,0 +1,22 @@
+#does everything work?
+import matplotlib.pyplot as plt
+import cv2
+
+
+mylist = [1, 2, 3, 4, "banana"]
+mylist.append("apple")
+mylist[1] = "orange"
+
+
+plt.plot(1, 2, 3, 4, 5, 6)
+#plt.show()
+
+print("OpenCV version:", cv2.__version__)
+print(mylist)
+
+Img = cv2.imread("test.jpg",0)
+
+cv2.imshow("Image", Img)
+
+cv2.waitKey(3000)
+cv2.destroyAllWindows()
