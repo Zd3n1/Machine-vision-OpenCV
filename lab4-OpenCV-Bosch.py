672b7693ad6edb8947dafaca38c5f87efa6d6f7a
Zd3n1 <109799398+Zd3n1@users.noreply.github.com>
2025-12-02 13:45:02 +0100

    fixed function resize function

--- lab4-OpenCV-Bosch.py
+++ lab4-OpenCV-Bosch.py
@@ -19,19 +19,19 @@ frame = cv2.imread('Bosch_image.tif')
 
 frame_s = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
 
-cv2.imshow('Resized image', frame_s)
+cv2.imshow('Resized_image', frame_s)
 
-cv2.createTrackbar('T', 'Resized image', 0, 255)
+cv2.createTrackbar('T', 'Resized_image', 0, 255, lambda x: None)
 
 
 #GRB 2 Gray
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #grayscale image
 
 
-t = cv2.getTrackbarPos('T', 'Resized image')
-_, thresh = cv2.threshold(gray, t, 255, cv2.THRESH_BINARY)
+t = cv2.getTrackbarPos('T', 'Resized_image')
+_, frame_t = cv2.threshold(gray, t, 255, cv2.THRESH_BINARY)
+cv2.imshow('Binary', frame_t)
 
 
-
-# cv2.waitKey()
-# cv2.destroyAllWindows()
+cv2.waitKey()
+cv2.destroyAllWindows()
